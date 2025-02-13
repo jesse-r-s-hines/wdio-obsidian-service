e39738baba8d47f96a610cd16d8f13f596c8a3bc
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-02-12 20:28:44 -0500

    Add script to launch obsidian

--- src/cli.ts
+++ src/cli.ts
@@ -1,22 +1,82 @@
 #!/bin/env node
+import path from "path"
 import { Command } from 'commander';
+import child_process from "child_process"
+import { setupConfigAndVault, ObsidianDownloader } from "./obsidianUtils.js"
+import { PluginEntry, ThemeEntry } from "./types.js";
 
 const program = new Command("obsidian-plugin-testing-library");
 
-const run = new Command("run")
-    .option('-s, --subtract')
-    .argument('<a>', 'A')
-    .argument('<b>', 'B')
-    .action((a, b, opts) => {
-        if (opts.subtract) {
-            console.log(`RUN ${a} - ${b} = ${(+a) - (+b)}`)
-        } else {
-            console.log(`RUN ${a} + ${b} = ${(+a) + (+b)}`)
-        }
+const launch = new Command("launch")
+    .argument('<vault>', 'Vault to open')
+    .option('-v, --version <version>', "Obsidian version to run", "latest")
+    .option('--installer-version <version>', "Obsidian installer version to run", "latest")
+    .option<string[]>('-p, --plugin <plugin>',
+        `Plugin to install. Format: "<path>" or "repo:<github-repo>" or "id:<community-id>". Can be repeated.`,
+        (curr, prev) => [...prev, curr], [],
+    )
+    .option<string[]>('-t, --theme <plugin>',
+        `Themes to install. Format: "<path>" or "repo:<github-repo>" or "name:<community-name>". Can be repeated but only last will be enabled.`,
+        (curr, prev) => [...prev, curr], [],
+    )
+    .option('--no-copy', "Don't copy the vault")
+    .action(async (vault, opts) => {
+        vault = path.resolve(vault)
+        const downloader = new ObsidianDownloader()
+        const { appVersionInfo, installerVersionInfo } = await downloader.resolveVersions(
+            opts.version, opts.installerVersion
+        );
+
+        const installerPath = await downloader.downloadInstaller(installerVersionInfo.version);
+        const appPath = await downloader.downloadApp(appVersionInfo.version);
+
+        const pluginEntries: PluginEntry[] = opts.plugin.map((p: string) => {
+            if (p.startsWith("id:")) {
+                return {id: p.slice(3)}
+            } else if (p.startsWith("repo:")) {
+                return {repo: p.slice(5)}
+            } else {
+                return {path: p}
+            }
+        })
+        const plugins = await downloader.downloadPlugins(pluginEntries);
+
+        const themeEntries: ThemeEntry[] = opts.theme.map((t: string, i: number) => {
+            let result: ThemeEntry
+            if (t.startsWith("name:")) {
+                result = {name: t.slice(5)}
+            } else if (t.startsWith("repo:")) {
+                result = {repo: t.slice(5)}
+            } else {
+                result = {path: t}
+            }
+            return {...result, enabled: i == opts.theme.length - 1}
+        })
+        const themes = await downloader.downloadThemes(themeEntries)
+        
+
+        const tmpDir = await setupConfigAndVault({
+            appVersion: appVersionInfo.version, installerVersion: installerVersionInfo.version,
+            appPath: appPath,
+            vault: vault, copyVault: opts.copy,
+            plugins: plugins, themes: themes,
+        });
+
+        // Spawn child and detach
+        const proc = child_process.spawn(installerPath, [
+            `--user-data-dir=${tmpDir}/config`,
+        ], {
+            detached: true,
+            stdio: 'ignore',
+        })
+        proc.unref()
+
+        console.log(`Launched obsidian ${appVersionInfo.version}`)
     })
 
 program
   .version(process.env.npm_package_version ?? "dev")
-  .addCommand(run)
+  .addCommand(launch)
 
 program.parse();
+
