27e60c09ef8ad4c12dd4f26ca13eaff6139bc5be
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-02-26 19:07:19 -0500

    Handle multiple windows

--- packages/wdio-obsidian-service/test/e2e/browserCommands.spec.ts
+++ packages/wdio-obsidian-service/test/e2e/browserCommands.spec.ts
@@ -2,6 +2,7 @@ import { browser } from '@wdio/globals'
 import { expect } from 'chai';
 import path from 'path';
 import fsAsync from "fs/promises"
+import { TFile } from 'obsidian';
 
 
 describe("Test custom browser commands", () => {
@@ -37,5 +38,31 @@ describe("Test custom browser commands", () => {
         await browser.enablePlugin("basic-plugin");
         plugins = await browser.executeObsidian(({app}) => [...(app as any).plugins.enabledPlugins].sort());
         expect(plugins).to.eql(["basic-plugin", "wdio-obsidian-service-plugin"]);
+    })
+
+    it('windows', async () => {
+        // pop-out windows have isolated window objects, check that executeObsidian still works and can access the
+        // globals.
+        await browser.executeObsidian(async ({app}) => {
+            await app.workspace.getLeaf('tab').openFile(app.vault.getAbstractFileByPath("Welcome.md") as TFile);
+            await app.workspace.getLeaf('tab').openFile(app.vault.getAbstractFileByPath("Goodbye.md") as TFile);
+        })
+        await browser.executeObsidianCommand("workspace:move-to-new-window");
+        const mainWindow = await browser.getWindowHandle();
+        const otherWindow = (await browser.getWindowHandles()).find(h => h != mainWindow)!;
+        await browser.switchToWindow(otherWindow);
+
+        const response = await browser.executeObsidian((obj) => {
+            return !!(
+                obj?.app &&
+                obj?.app?.workspace &&
+                obj?.obsidian &&
+                obj?.obsidian.App
+            )
+        })
+        expect(response).to.eql(true);
+
+        await browser.switchToWindow(mainWindow);
+        await browser.executeObsidian(({app}) => { app.workspace.detachLeavesOfType("markdown") })
     })
 })
