8d8c88208be1f55435e927d9920ae3c9550ade6e
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-03-14 19:40:00 -0400

    Add some helper functions

--- packages/wdio-obsidian-service/src/index.ts
+++ packages/wdio-obsidian-service/src/index.ts
@@ -13,12 +13,30 @@ export type {
     LocalThemeEntry, GitHubThemeEntry, CommunityThemeEntry, ThemeEntry,
 } from "obsidian-launcher";
 
+
+// Some convenience helpers for use in wdio.conf.ts
+
 /**
- * Returns true if there's currently an Obsidian beta and we have the credentials to download it or it's already in the
- * cache.
+ * Returns true if there is a current Obsidian beta and we have the credentials to download it, or its already in cache.
  */
 export async function obsidianBetaAvailable(cacheDir?: string) {
     const launcher = new ObsidianLauncher({cacheDir: cacheDir});
     const versionInfo = await launcher.getVersionInfo("latest-beta");
     return versionInfo.isBeta && await launcher.isAvailable(versionInfo.version);
+}
+
+/**
+ * Resolves Obsidian app and installer version strings to absolute versions.
+ * @param appVersion Obsidian version string or "latest", "latest-beta" or "earliest". "earliest" will use the 
+ *     minAppVersion set in your manifest.json.
+ * @param installerVersion Obsidian version string or "latest" or "earliest". "earliest" will use the minimum
+ *     installer version compatible with the appVersion.
+ * @returns [appVersion, installerVersion] with any "latest" etc. resolved to specific versions.
+ */
+export async function resolveObsidianVersions(
+    appVersion: string, installerVersion: string, cacheDir?: string,
+): Promise<[string, string]> {
+    const launcher = new ObsidianLauncher({cacheDir: cacheDir});
+    return await launcher.resolveVersions(appVersion, installerVersion);
+
 }
