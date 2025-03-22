9b65f5ad277dafee663023932eb63cfcfbd35fe2
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-03-22 10:23:37 -0400

    Fix require

--- packages/wdio-obsidian-service/src/pageobjects/obsidianPage.ts
+++ packages/wdio-obsidian-service/src/pageobjects/obsidianPage.ts
@@ -164,7 +164,8 @@ class ObsidianPage {
             .value();
 
         await browser.executeObsidian(async ({app, obsidian}, newVaultFolders, newVaultFiles) => {
-            const fs = require('fs');
+            // the require is getting transpiled by tsup, so pull it directly from wdioObsidianService
+            const fs = (window as any).wdioObsidianService.require('fs');
 
             // "rsync" the vault
             for (const newFolder of newVaultFolders) {
