dbb2543b68924adaf7a7ddefcedf582a69dd545d
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-05-13 20:34:10 -0400

    Add test for standalone mode

---
+++ packages/wdio-obsidian-service/test/standalone/standalone.spec.ts
@@ -0,0 +1,39 @@
+import path from "path";
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import { startWdioSession } from "wdio-obsidian-service"
+import { fileURLToPath } from "url"
+
+const workspacePath = path.resolve(path.join(fileURLToPath(import.meta.url), "../../../../.."))
+const cacheDir = path.join(workspacePath, ".obsidian-cache");
+
+describe("standalone mode test", function() {
+    let browser: WebdriverIO.Browser|undefined;
+    before(async function() {
+        this.timeout("10m");
+        browser = await startWdioSession({
+            capabilities: {
+                browserName: "obsidian",
+                browserVersion: "latest",
+                'wdio:obsidianOptions': {
+                    installerVersion: "latest",
+                    plugins: [
+                        "./test/plugins/basic-plugin",
+                    ],
+                    vault: "./test/vaults/basic",
+                },
+            },
+            cacheDir: cacheDir,
+            logLevel: "warn",
+        })
+    });
+    after(async function() {
+        await browser?.deleteSession();
+    });
+    this.timeout("30s");
+
+    it("basic", async function() {
+        const vaultPath = await browser!.executeObsidian(({app}) => (app.vault.adapter as any).getBasePath());
+        expect(path.basename(vaultPath)).to.match(/^basic-/);
+    });
+})
