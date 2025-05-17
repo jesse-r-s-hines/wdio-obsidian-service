5ba5ff8bf1d0b3615e0f19d34417cbffca9ebd6b
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-05-17 18:28:37 -0400

    Improve eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,18 +4,38 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     eslint.configs.recommended,
-    tseslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
     {
         rules: {
             "no-prototype-builtins": "off",
+            'no-undef': 'off',
             "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unsafe-argument": "off",
+            "@typescript-eslint/no-unsafe-assignment": "off",
+            "@typescript-eslint/no-unsafe-member-access": "off",
+            "@typescript-eslint/no-unsafe-return": "off",
+            "@typescript-eslint/no-unsafe-call": "off",
             "@typescript-eslint/no-namespace": "off",
             '@typescript-eslint/no-require-imports': 'off',
-            'no-undef': 'off',
+            "@typescript-eslint/restrict-plus-operands": "off",
+            "@typescript-eslint/require-await": "off",
+            "@typescript-eslint/no-empty-object-type": "off",
+            "@typescript-eslint/restrict-template-expressions": "off",
+            "@typescript-eslint/no-misused-promises": ["error", {
+                checksVoidReturn: false,
+            }],
             "@typescript-eslint/no-unused-vars": ["error", {
-                "varsIgnorePattern": "^_",
-                "destructuredArrayIgnorePattern": ".*",
-                "ignoreRestSiblings": true,
+                varsIgnorePattern: "^_",
+                destructuredArrayIgnorePattern: ".*",
+                ignoreRestSiblings: true,
             }],
         },
     }
