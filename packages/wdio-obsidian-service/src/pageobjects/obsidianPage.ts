0aec4811e280d020c7a80b8f1b0915343adb24d3
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-05-25 19:42:55 -0400

    Better errors when layout doesn't exist

--- packages/wdio-obsidian-service/src/pageobjects/obsidianPage.ts
+++ packages/wdio-obsidian-service/src/pageobjects/obsidianPage.ts
@@ -158,6 +158,8 @@ class ObsidianPage extends BasePage {
                 const fileContent = await fsAsync.readFile(workspacesPath, 'utf-8');
                 layout = JSON.parse(fileContent)?.workspaces?.[layoutName];
             } catch {
+            }
+            if (!layout) {
                 throw new Error(`No workspace ${layoutName} found in ${configDir}/workspaces.json`);
             }
         }
