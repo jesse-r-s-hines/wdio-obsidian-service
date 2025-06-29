c1cede0c4aef494b24aceb6cdff99f52d08063ab
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-06-29 17:22:16 -0400

    Open vault sorta working

--- packages/wdio-obsidian-service/test/e2e/basic.spec.ts
+++ packages/wdio-obsidian-service/test/e2e/basic.spec.ts
@@ -6,9 +6,14 @@ import { sleep } from '../../src/utils.js';
 
 describe("Basic obsidian launch", () => {
     it('Basic', async () => {
-       const elem = await browser.$("body");
-        // // console.log(elem, await elem.getHTML())
-        const localStorage = await browser.execute(() => {
+        // console.log({
+        //     settings: await browser.getSettings(),
+        //     context: await browser.getContext({returnDetailedContext: true}),
+        //     contexts: await browser.getContexts({returnDetailedContexts: true, isAndroidWebviewVisible: false}),
+        // })
+        // const elem = await browser.$(".status-bar");
+        // console.log(elem, await elem.getHTML())
+        console.log("localstorage in test", await browser.execute(async () => {
             const rand = Math.trunc(Math.random() * 1_000_000).toString();
             localStorage.setItem(`foo-${rand}`, rand);
             const data: any = {}
@@ -18,8 +23,29 @@ describe("Basic obsidian launch", () => {
                 data[key] = value;
             }
             return data;
-        });
-        console.log({localStorage});
-        await sleep(1000 * 1000);
+        }));
     })
+
+    // it('Vault opened', async () => {
+    //     const vaultPath = await browser.executeObsidian(({app}) => (app.vault.adapter as any).getBasePath());
+
+    //     // Should have created a copy of vault
+    //     expect(path.basename(vaultPath)).toMatch(/^basic-/)
+
+    //     const vaultFiles = await browser.executeObsidian(({app}) =>dd
+    //         app.vault.getMarkdownFiles().map(x => x.path).sort()
+    //     );
+    //     expect(vaultFiles).toEqual(["Goodbye.md", "Welcome.md"]);
+    // })
+
+    it("Wait", async () => {
+        while (true) {
+            // await browser.execute(() => { return 1 });
+            await browser.getSettings()
+            await sleep(10 * 1000);
+        }
+    });
 })
+
+
+
--- packages/wdio-obsidian-service/wdio.conf.ts
+++ packages/wdio-obsidian-service/wdio.conf.ts
@@ -8,6 +8,7 @@ import { ObsidianVersionInfo } from "obsidian-launcher"
 import _ from "lodash"
 import type { Capabilities, Options, Services } from '@wdio/types'
 import crypto from "crypto";
+import { sleep } from "./src/utils.js";
 
 const workspacePath = path.resolve(fileURLToPath(import.meta.url), "../../..");
 const cacheDir = path.join(workspacePath, ".obsidian-cache");
@@ -24,13 +25,15 @@ export const config: WebdriverIO.Config = {
         platformName: 'Android',
         'appium:automationName': 'UiAutomator2',
         // 'appium:appPackage': 'com.android.settings',
-        'appium:app': "/home/jesse/Downloads/apk/Obsidian-1.8.10.apk",
+        'appium:app': "/home/jesse/Downloads/apk/Obsidian-1.8.10-debuggable.apk",
         'appium:deviceName': "Pixel_9",
         'appium:platformVersion': "15.0",
         'appium:avd': "Pixel_9",
-        'appium:autoWebview': true,
+        // 'appium:autoWebview': true,
         'appium:fullReset': true,
         'appium:chromedriverExecutableDir': path.join(cacheDir, "./appium-chromedriver"),
+        'appium:autoGrantPermissions': true,
+        // 'appium:permissions': [],
     }],
 
     services: [
@@ -62,20 +65,40 @@ export const config: WebdriverIO.Config = {
     },
 
     async before(capabilities: WebdriverIO.Capabilities, specs: unknown, browser: WebdriverIO.Browser) {
+        await browser.execute("mobile: changePermissions", {
+            action: "grant",
+            appPackage: "md.obsidian",
+            permissions: "all",
+            // target: "appops", // requires adb_shell apparently?
+        })
+
+
+        await browser.execute("mobile: changePermissions", {
+            action: "allow",
+            // appPackage: "md.obsidian",
+            permissions: [
+                "MANAGE_EXTERNAL_STORAGE",
+                "READ_EXTERNAL_STORAGE",
+                "WRITE_EXTERNAL_STORAGE",
+            ],
+            target: "appops", // requires adb_shell apparently?
+        })
+
         const vaultPath = path.join(workspacePath, 'packages/wdio-obsidian-service/test/vaults/basic');
         const mobileVaultsDir = "/storage/emulated/0/Documents/wdio-obsidian-service-vaults"
 
         // this requires the allow-insecure adb_shell arg
         // There is also a "mobile: deleteFile" option, but it seems to be bugged and doesn't
         // work on folders. See https://github.com/appium/appium-android-driver/issues/1003
-        await browser.executeScript("mobile: shell", [{
+        await browser.execute("mobile: shell", {
             command: "rm",
             args: ["-rf", mobileVaultsDir],
             includeStderr: true,
-        }])
+        })
 
-        const slug = crypto.randomBytes(8).toString("base64url").replace(/[-_]/g, '0');
-        const mobileVaultPath = path.join(mobileVaultsDir, `${path.basename(vaultPath)}-${slug}`)
+        // const slug = crypto.randomBytes(8).toString("base64url").replace(/[-_]/g, '0');
+        // const mobileVaultPath = path.join(mobileVaultsDir, `${path.basename(vaultPath)}-${slug}`)
+        const mobileVaultPath = path.join(mobileVaultsDir, `${path.basename(vaultPath)}`)
         
         const vaultFiles = await fsAsync.readdir(vaultPath, {recursive: true, withFileTypes: true});
         for (const file of vaultFiles) {
@@ -85,5 +108,24 @@ export const config: WebdriverIO.Config = {
                 await browser.pushFile(path.join(mobileVaultPath, filePath), contents.toString('base64'));
             }
         }
+
+        for (const file of vaultFiles) {
+            const filePath = path.relative(vaultPath, path.join(file.parentPath, file.name));
+            if (file.isFile()) {
+                const contents = await fsAsync.readFile(path.join(vaultPath, filePath))
+                await browser.pushFile(path.join(mobileVaultPath + "2", filePath), contents.toString('base64'));
+            }
+        }
+
+        await browser.switchContext("WEBVIEW_md.obsidian");
+        await browser.execute(async (mobileVaultPath) => {
+            localStorage.setItem('mobile-external-vaults', JSON.stringify([mobileVaultPath]));
+            localStorage.setItem('mobile-selected-vault', mobileVaultPath);
+        }, mobileVaultPath);
+
+        await sleep(1000) // have to sleep here or localStorage doesn't get persisted. TODO: Figure out a better way
+        await browser.switchContext("NATIVE_APP");
+        await browser.relaunchActiveApp();
+        await browser.switchContext("WEBVIEW_md.obsidian");
     }
 }
