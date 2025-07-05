95b4e1592801b49b33b6eab3e6b85104d6bfa3cb
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-07-05 16:50:32 -0400

    Add helper function

--- packages/wdio-obsidian-service/src/utils.ts
+++ packages/wdio-obsidian-service/src/utils.ts
@@ -1,3 +1,7 @@
 export async function sleep(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export function quote(input: string) {
+    return `'${input.replace(/'/g, "'\\''")}'`;
+}
