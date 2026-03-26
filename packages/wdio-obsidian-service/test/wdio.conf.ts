4cf2c8d545c1a451b2d3e91c551569858aca2fda
Jesse Hines <jesse.r.s.hines@gmail.com>
2026-03-26 19:19:37 -0400

    Factor out shared wdio config

--- packages/wdio-obsidian-service/test/wdio.conf.ts
+++ packages/wdio-obsidian-service/test/wdio.conf.ts
@@ -1,39 +1,12 @@
 import { minSupportedObsidianVersion } from "wdio-obsidian-service"
-import ObsidianLauncher from "obsidian-launcher";
-import { pathToFileURL, fileURLToPath } from "url"
-import path from "path"
-import fsAsync from "fs/promises"
 import semver from "semver";
-import { ObsidianVersionInfo } from "obsidian-launcher"
 import _ from "lodash"
+import {
+    minorVersion, obsidianBetaAvailable, cacheDir, obsidianServiceOptions, getCapabilities, config as sharedConfig,
+    allVersions,
+} from "./wdio.shared.conf.js";
 
-const maxInstances = Number(process.env['WDIO_MAX_INSTANCES'] ?? 4);
-const workspacePath = path.resolve(fileURLToPath(import.meta.url), "../../../..")
-const obsidianVersionsJson = path.join(workspacePath, "obsidian-versions.json");
-const allVersions: ObsidianVersionInfo[] = JSON.parse(await fsAsync.readFile(obsidianVersionsJson, 'utf-8')).versions;
 const minInstallerVersion = allVersions.find(v => v.version == minSupportedObsidianVersion)!.minInstallerVersion!;
-const cacheDir = path.join(workspacePath, ".obsidian-cache");
-const obsidianServiceOptions = {
-    versionsUrl: pathToFileURL(obsidianVersionsJson).toString(),
-}
-
-/**
- * Can't use the regular obsidianBetaAvailable as it fetches the obsidian-versions.json instead of using the local one
- */
-async function obsidianBetaAvailable(cacheDir?: string) {
-    const launcher = new ObsidianLauncher({
-        cacheDir: cacheDir,
-        versionsUrl: obsidianServiceOptions.versionsUrl,
-    });
-    const versionInfo = await launcher.getVersionInfo("latest-beta");
-    return versionInfo.isBeta && await launcher.isAvailable(versionInfo.version);
-}
-
-function minorVersion(v: string) {
-    return v.split(".").slice(0, 2).join('.')
-};
-
-const defaultTestLevel = process.env.TEST_LEVEL ?? 'standard';
 
 let versionsToTest: [string, string, string?][] // [app, installer, testLevel]
 if (process.env.OBSIDIAN_VERSIONS == "all") {
@@ -73,7 +46,7 @@ if (process.env.OBSIDIAN_VERSIONS == "all") {
 } else if (process.env.OBSIDIAN_VERSIONS) {
     // Space separated list of appVersion/installerVersion, e.g. "1.7.7/latest latest/earliest"
     versionsToTest = process.env.OBSIDIAN_VERSIONS.split(/[ ,]+/).map(v => {
-        let [app, installer = "earliest", testLevel = defaultTestLevel] = v.split("/");
+        let [app, installer = "earliest", testLevel] = v.split("/");
         app = app == "earliest" ? minSupportedObsidianVersion : app;
         return [app, installer, testLevel];
     })
@@ -81,32 +54,21 @@ if (process.env.OBSIDIAN_VERSIONS == "all") {
     versionsToTest = [[minSupportedObsidianVersion, "earliest"], ["latest", "latest"]];
 }
 
-export const config: WebdriverIO.Config = {
-    runner: 'local',
-
+export const config: WebdriverIO.Config = _.merge({}, sharedConfig, {
     // How many instances of Obsidian should be launched in parallel during testing.
-    maxInstances: maxInstances,
-    specs: ['./e2e/**/*.ts'],
+    maxInstances: Number(process.env['WDIO_MAX_INSTANCES'] ?? 4),
 
-    capabilities: versionsToTest.flatMap(([appVersion, installerVersion, testLevel = defaultTestLevel]) => {
-        const excludeBasic = 'e2e/basic.spec.ts';
-        const excludeRest = '!(basic.spec.ts)';
-
-        const cap: WebdriverIO.Capabilities = {
-            browserName: "obsidian",
+    capabilities: versionsToTest.flatMap(([appVersion, installerVersion, testLevel]) => [
+        ...getCapabilities(testLevel, {
             browserVersion: appVersion,
             'wdio:obsidianOptions': {
                 installerVersion: installerVersion,
-                plugins: [
-                    "./plugins/basic-plugin",
-                ],
-                themes: [
-                    "./themes/basic-theme",
-                ],
             },
-        }
-        const emulateMobileOptions: WebdriverIO.Capabilities = {
+        }),
+        ...getCapabilities(testLevel, {
+            browserVersion: appVersion,
             'wdio:obsidianOptions': {
+                installerVersion: installerVersion,
                 emulateMobile: true,
             },
             'goog:chromeOptions': {
@@ -114,43 +76,14 @@ export const config: WebdriverIO.Config = {
                     deviceMetrics: {width: 390, height: 844}
                 },
             },
-        }
-        const caps: WebdriverIO.Capabilities[] = [
-            _.merge({}, cap, { // separate capability for basic tests, test passing vault in the capability
-                'wdio:exclude': [excludeRest], // --spec command overrides wdio:specs, so use wdio:exclude instead
-                'wdio:obsidianOptions': { vault: './vaults/basic' },
-            }),
-            _.merge({}, cap, emulateMobileOptions, {
-                'wdio:exclude': [excludeRest],
-                'wdio:obsidianOptions': { vault: './vaults/basic' },
-            }),
-        ]
-        if (testLevel != 'basic') {
-            caps.push(
-                _.merge({}, cap, { 'wdio:exclude': [excludeBasic] }),
-                _.merge({}, cap, emulateMobileOptions, { 'wdio:exclude': [excludeBasic] }),
-            )
-        }
-        return caps;
-    }),
-
-    services: [["obsidian", obsidianServiceOptions]],
-
-    cacheDir: cacheDir,
-
-    framework: 'mocha',
-    
-    reporters: ["obsidian"],
+        })
+    ]),
 
     bail: 3,
-
     mochaOpts: {
-        ui: 'bdd',
         timeout: 60 * 1000,
         bail: 3,
     },
 
-    logLevel: "warn",
-
-    injectGlobals: false,
-}
+    services: [["obsidian", obsidianServiceOptions]],
+})
