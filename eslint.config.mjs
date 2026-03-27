11fca8737705f5f52c280cc7f0f97dc4288d58e7
Jesse Hines <jesse.r.s.hines@gmail.com>
2026-03-26 20:19:22 -0400

    Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,45 +1,52 @@
 // @ts-check
 import eslint from '@eslint/js';
+import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
+export default defineConfig([
+    globalIgnores(["**/node_modules", "**/dist", "**/tsup.config.ts"]),
     {
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
+        files: ["packages/**/*.ts"],
+        extends: [
+            eslint.configs.recommended,
+            tseslint.configs.recommendedTypeChecked,
+            {
+                languageOptions: {
+                    parserOptions: {
+                        projectService: true,
+                        tsconfigRootDir: import.meta.dirname,
+                    },
+                },
             },
-        },
+            {
+                rules: {
+                    "no-prototype-builtins": "off",
+                    'no-undef': 'off',
+                    "prefer-const": ["error", {
+                        "destructuring": "all",
+                    }],
+                    "@typescript-eslint/no-explicit-any": "off",
+                    "@typescript-eslint/no-unsafe-argument": "off",
+                    "@typescript-eslint/no-unsafe-assignment": "off",
+                    "@typescript-eslint/no-unsafe-member-access": "off",
+                    "@typescript-eslint/no-unsafe-return": "off",
+                    "@typescript-eslint/no-unsafe-call": "off",
+                    "@typescript-eslint/no-namespace": "off",
+                    '@typescript-eslint/no-require-imports': 'off',
+                    "@typescript-eslint/restrict-plus-operands": "off",
+                    "@typescript-eslint/require-await": "off",
+                    "@typescript-eslint/no-empty-object-type": "off",
+                    "@typescript-eslint/restrict-template-expressions": "off",
+                    "@typescript-eslint/no-misused-promises": ["error", {
+                        checksVoidReturn: false,
+                    }],
+                    "@typescript-eslint/no-unused-vars": ["error", {
+                        varsIgnorePattern: "^_",
+                        destructuredArrayIgnorePattern: ".*",
+                        ignoreRestSiblings: true,
+                    }],
+                },
+            }
+        ],
     },
-    {
-        rules: {
-            "no-prototype-builtins": "off",
-            'no-undef': 'off',
-            "prefer-const": ["error", {
-                "destructuring": "all",
-            }],
-            "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-unsafe-argument": "off",
-            "@typescript-eslint/no-unsafe-assignment": "off",
-            "@typescript-eslint/no-unsafe-member-access": "off",
-            "@typescript-eslint/no-unsafe-return": "off",
-            "@typescript-eslint/no-unsafe-call": "off",
-            "@typescript-eslint/no-namespace": "off",
-            '@typescript-eslint/no-require-imports': 'off',
-            "@typescript-eslint/restrict-plus-operands": "off",
-            "@typescript-eslint/require-await": "off",
-            "@typescript-eslint/no-empty-object-type": "off",
-            "@typescript-eslint/restrict-template-expressions": "off",
-            "@typescript-eslint/no-misused-promises": ["error", {
-                checksVoidReturn: false,
-            }],
-            "@typescript-eslint/no-unused-vars": ["error", {
-                varsIgnorePattern: "^_",
-                destructuredArrayIgnorePattern: ".*",
-                ignoreRestSiblings: true,
-            }],
-        },
-    }
-);
+]);
